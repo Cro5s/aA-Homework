1d5b9505e4129bbbb03b24172a6c82a87ec51a38
Kadeem Jackson <kdjackson35@gmail.com>
2020-01-15 03:13:31 -0500

    N+1 Buster Final

--- W8D3/N+1 Buster/app/models/house.rb
+++ W8D3/N+1 Buster/app/models/house.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: houses
+#
+#  id         :integer          not null, primary key
+#  address    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class House < ApplicationRecord
   has_many :gardeners,
     class_name: 'Gardener',
@@ -20,5 +30,14 @@ class House < ApplicationRecord
 
   def better_seeds_query
     # TODO: your code here
+    plants = self.plants.includes(:seeds)
+    
+    seeds = []
+
+    plants.each do |plant|
+      seeds << plant.seeds
+    end
+
+    seeds
   end
 end
